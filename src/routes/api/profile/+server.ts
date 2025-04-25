5119bf0dc041ad16f1d0ef81729d389514eb0551
Andrew K <andrewjk@users.noreply.github.com>
2025-04-25 12:38:13 +1000

    Fix: profile should be visible to everyone

--- src/routes/api/profile/+server.ts
+++ src/routes/api/profile/+server.ts
@@ -1,14 +1,8 @@
 import profileGet from "@/lib/profile/profileGet";
 import type { ServerEndPoint } from "@torpor/build";
-import { unauthorized } from "@torpor/build/response";
 
 export default {
-	get: async ({ appData }) => {
-		const user = appData.user;
-		if (!user) {
-			return unauthorized();
-		}
-
+	get: async () => {
 		return await profileGet();
 	},
 } satisfies ServerEndPoint;
