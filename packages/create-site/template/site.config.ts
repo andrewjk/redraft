4912366ed08a20634a16db9c3fe593fc03367b9a
Andrew K <andrewjk@users.noreply.github.com>
2025-04-30 12:27:00 +1000

    Refactor: rename the adapter

--- packages/create-site/template/site.config.ts
+++ packages/create-site/template/site.config.ts
@@ -1,11 +1,11 @@
 import { node as socialAdapter } from "@redraft/adapter-node";
 import { defineSite } from "@redraft/site";
-import { node } from "@torpor/adapter-node";
+import { node as siteAdapter } from "@torpor/adapter-node";
 import { Site } from "@torpor/build";
 import "dotenv/config";
 
 const site: Site = new Site();
-site.adapter = node;
+site.adapter = siteAdapter;
 
 await defineSite(site, socialAdapter);
 
