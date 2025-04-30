1d25a3090a04962ffeb408f136b6d2be3431a305
Andrew K <andrewjk@users.noreply.github.com>
2025-04-30 12:24:20 +1000

    Chore: update dependencies

--- packages/site/drizzle.config.ts
+++ packages/site/drizzle.config.ts
@@ -1,12 +1,10 @@
 import { type Config, defineConfig } from "drizzle-kit";
 
-const config: Config = defineConfig({
+export default defineConfig({
 	out: "./drizzle",
 	schema: "./src/data/schema",
 	dialect: "sqlite",
 	dbCredentials: {
 		url: process.env.DB_CONNECTION!,
 	},
-});
-
-export default config;
+}) satisfies Config as Config;
