6fc7f96cfdcef70993b71125af1b6c564cac5e48
Andrew K <andrewjk@users.noreply.github.com>
2025-05-07 17:40:52 +1000

    Feat: load url, name and image in meta
    For displaying in the extension

--- packages/site/src/routes/_layout.server.ts
+++ packages/site/src/routes/_layout.server.ts
@@ -1,27 +1,33 @@
 import type { PageServerEndPoint } from "@torpor/build";
 import { ok } from "@torpor/build/response";
+import database from "../data/database";
 import env from "../lib/env";
 
 export default {
-	load: ({ appData, params }) => {
+	load: async ({ appData, params }) => {
 		const user = appData.user;
 		const follower = appData.follower;
-		const url = env().SITE_LOCATION;
+
+		const db = database();
+		const currentUser = await db.query.usersTable.findFirst({
+			columns: { name: true, image: true },
+		});
+
 		return ok({
-			url,
+			url: env().SITE_LOCATION,
+			name: currentUser?.name,
+			image: currentUser?.image,
 			username: params.user,
 			base: params.user ? `/${params.user}/` : "/",
 			user: user && {
 				url: user.url,
 				name: user.name,
 				image: user.image,
-				bio: user.bio,
 			},
 			follower: follower && {
 				url: follower.url,
 				name: follower.name,
 				image: follower.image,
-				bio: follower.bio,
 			},
 		});
 	},
