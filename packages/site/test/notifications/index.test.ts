fd78a0230a58325ee9c1965cda742ada6f8a25ec
Andrew K <andrewjk@users.noreply.github.com>
2025-05-13 11:31:17 +1000

    Feat: notifications

--- packages/site/test/notifications/index.test.ts
+++ packages/site/test/notifications/index.test.ts
@@ -1,11 +1,13 @@
 import { queryByText } from "@testing-library/dom";
 import "@testing-library/jest-dom/vitest";
 import { Site } from "@torpor/build";
+import { ServerEvent } from "@torpor/build/server";
 import { runTest } from "@torpor/build/test";
 import type { LibSQLDatabase } from "drizzle-orm/libsql";
 import { afterAll, beforeAll, expect, test } from "vitest";
 import * as schema from "../../src/data/schema/index";
 import notificationList from "../../src/lib/profile/notificationList";
+import setUserToken from "../../src/lib/utils/setUserToken";
 import mockFetch from "../mockFetch";
 import { cleanUpSiteTest, prepareSiteTest } from "../prepareSiteTest";
 
@@ -21,10 +23,19 @@ afterAll(() => {
 });
 
 test("notifications get", async () => {
-	/* TODO: Test with auth
 	mockFetch(fetch, notificationList("xxx-alice"));
 
-	const response = await runTest(site, "/profile/notifications");
+	let ev = new ServerEvent(new Request(`http://localhost/profile/notifications`));
+	setUserToken(ev.cookies, {
+		url: "",
+		username: "",
+		name: "",
+		image: "",
+		token: "",
+		code: "",
+	});
+
+	const response = await runTest(site, "/profile/notifications", ev);
 	const html = await response.text();
 
 	const div = document.createElement("div");
@@ -32,5 +43,4 @@ test("notifications get", async () => {
 
 	const text = queryByText(div, "Someone commented on your post");
 	expect(text).not.toBeNull();
-	*/
 });
