c8ac7bf22c132243fe17f3492ccffd69ef530f79
Andrew K <andrewjk@users.noreply.github.com>
2025-06-10 13:06:06 +1000

    Feat: store the post input outside the view
    So that it won't be discarded on navigation

---
+++ .changeset/good-wolves-clap.md
@@ -0,0 +1,5 @@
+---
+"@redraft/site": patch
+---
+
+Feat: store the post input outside the view
---
+++ packages/site/src/views/$postInput.ts
@@ -0,0 +1,12 @@
+import { $watch } from "@torpor/view";
+
+/**
+ * The shared data for a post that is being written, so that we can keep it across pages
+ */
+const $postInput = $watch({
+	id: -1,
+	type: 0,
+	visibility: 0,
+});
+
+export default $postInput;
