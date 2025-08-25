e0178dbc7a250333e3293872b7ddd0d1269831d8
Andrew K <andrewjk@users.noreply.github.com>
2025-08-25 14:18:27 +1000

    Fix: defer_foreign_keys seems to work in more places

--- packages/site/src/data/migrations/0014_tearful_tomorrow_man.sql
+++ packages/site/src/data/migrations/0014_tearful_tomorrow_man.sql
@@ -9,7 +9,7 @@ CREATE TABLE `events` (
 	`deleted_at` integer
 );
 --> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
+PRAGMA defer_foreign_keys=true;--> statement-breakpoint
 CREATE TABLE `__new_feed` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`user_id` integer,
@@ -44,7 +44,8 @@ CREATE TABLE `__new_feed` (
 INSERT INTO `__new_feed`("id", "user_id", "slug", "text", "comment_count", "last_comment_at", "visibility", "image", "image_alt_text", "link_type", "link_url", "link_title", "link_image", "link_publication", "link_embed_src", "link_embed_width", "link_embed_height", "rating_value", "rating_bound", "child_count", "published_at", "republished_at", "liked", "saved", "emoji", "created_at", "updated_at", "deleted_at") SELECT "id", "user_id", "slug", "text", "comment_count", "last_comment_at", "visibility", "image", "image_alt_text", "link_type", "link_url", "link_title", "link_image", "link_publication", "link_embed_src", "link_embed_width", "link_embed_height", "rating_value", "rating_bound", "child_count", "published_at", "republished_at", "liked", "saved", "emoji", "created_at", "updated_at", "deleted_at" FROM `feed`;--> statement-breakpoint
 DROP TABLE `feed`;--> statement-breakpoint
 ALTER TABLE `__new_feed` RENAME TO `feed`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
+PRAGMA defer_foreign_keys=false;--> statement-breakpoint
+PRAGMA defer_foreign_keys=true;--> statement-breakpoint
 CREATE TABLE `__new_posts` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`slug` text NOT NULL,
@@ -84,4 +85,5 @@ CREATE TABLE `__new_posts` (
 INSERT INTO `__new_posts`("id", "slug", "text", "comment_count", "last_comment_at", "like_count", "emoji_first", "emoji_second", "emoji_third", "visibility", "list_id", "image", "image_alt_text", "link_type", "link_url", "link_title", "link_image", "link_publication", "link_embed_src", "link_embed_width", "link_embed_height", "article_id", "event_id", "rating_value", "rating_bound", "parent_id", "child_count", "pinned", "published_at", "republished_at", "created_at", "updated_at", "deleted_at") SELECT "id", "slug", "text", "comment_count", "last_comment_at", "like_count", "emoji_first", "emoji_second", "emoji_third", "visibility", "list_id", "image", "image_alt_text", "is_article", "link_url", "link_title", "link_image", "link_publication", "link_embed_src", "link_embed_width", "link_embed_height", "article_id", NULL, "rating_value", "rating_bound", "parent_id", "child_count", "pinned", "published_at", "republished_at", "created_at", "updated_at", "deleted_at" FROM `posts`;--> statement-breakpoint
 DROP TABLE `posts`;--> statement-breakpoint
 ALTER TABLE `__new_posts` RENAME TO `posts`;
+PRAGMA defer_foreign_keys=false;--> statement-breakpoint
 UPDATE `posts` SET "link_type" = NULL WHERE "link_type" = 0 AND link_url IS NULL;
