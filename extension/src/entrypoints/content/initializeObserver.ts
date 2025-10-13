34bfb14cc21f5e81fcdf9bd09732c5002ead24e0
Andrew K <andrewjk@users.noreply.github.com>
2025-10-13 15:29:33 +1100

    Chore: update torpor

--- extension/src/entrypoints/content/initializeObserver.ts
+++ extension/src/entrypoints/content/initializeObserver.ts
@@ -1,7 +1,7 @@
 import { formatContent } from "./formatContent";
 
 export function initializeObserver(): void {
-	// HACK: We don't currently send <:head> elements with SSR, so we need to
+	// HACK: We don't currently send @head elements with SSR, so we need to
 	// listen for changes from when the page is hydrated. This may change...
 	const targetNode = document.head;
 	const config = { childList: true };
