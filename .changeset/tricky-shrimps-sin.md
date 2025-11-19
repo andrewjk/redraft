ab669b0937d3ef326c74a4179b42d787de4f49a8
Andrew K <andrewjk@users.noreply.github.com>
2025-11-19 15:06:10 +1100

    Fix: add the api routes

---
+++ .changeset/tricky-shrimps-sin.md
@@ -0,0 +1,5 @@
+---
+"@redraft/site": patch
+---
+
+Fix: add the api routes
--- packages/site/src/index.ts
+++ packages/site/src/index.ts
@@ -2,7 +2,11 @@ import { type Adapter } from "@redraft/adapter-core";
 import { Site, type Adapter as SiteAdapter } from "@torpor/build";
 
 export async function defineSite(site: Site, adapter: Adapter, siteAdapter: SiteAdapter) {
+	// Add the routes
 	await site.addRouteFolder("node_modules/@redraft/site/src/routes");
+
+	// Add the API routes, which go under the /api route, and have their own hooks etc
+	await site.addRouteFolder("node_modules/@redraft/site/src/api", "/api");
 
 	site.adapter = siteAdapter;
 
