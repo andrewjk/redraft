d165a522a5c8a5c8dce6ca8f1711bffe3bbe0ea6
Andrew K <andrewjk@users.noreply.github.com>
2025-12-15 14:48:40 +1100

    Fix: use @torpor/ui in dev

---
+++ .changeset/strong-chefs-itch.md
@@ -0,0 +1,5 @@
+---
+"@redraft/site": patch
+---
+
+Fix: use @torpor/ui in dev
--- packages/site/src/index.ts
+++ packages/site/src/index.ts
@@ -2,13 +2,21 @@ import { type Adapter } from "@redraft/adapter-core";
 import { Site, type Adapter as SiteAdapter } from "@torpor/build";
 
 export async function defineSite(site: Site, adapter: Adapter, siteAdapter: SiteAdapter) {
+	site.adapter = siteAdapter;
+
+	// HACK: To be able to import `.torp` files from barrel files in node_modules,
+	// we need to add their libraries to `ssr.noExternal`
+	site.viteConfig = {
+		ssr: {
+			noExternal: ["@torpor/ui"],
+		},
+	};
+
 	// Add the routes
 	await site.addRouteFolder("node_modules/@redraft/site/src/routes");
 
 	// Add the API routes, which go under the /api route, and have their own hooks etc
 	await site.addRouteFolder("node_modules/@redraft/site/src/api", "/api");
-
-	site.adapter = siteAdapter;
 
 	// For development. In production it should get built by the adapter
 	// @ts-ignore
